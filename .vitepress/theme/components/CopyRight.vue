7bdd1c13cfa87bc402a40f59c840e4581ce55dc4
HerbertHe <Herbert.He0229@gmail.com>
2024-01-16 10:20:06 +0800

    chore :wrench: : update cc license

--- .vitepress/theme/components/CopyRight.vue
+++ .vitepress/theme/components/CopyRight.vue
@@ -1,8 +1,27 @@
 <template>
   <footer>
-    Copyright &copy; 2018-{{ date }} <a href="https://github.com/HerbertHe" target="_blank" title="作者">Herbert He</a> All
-    Rights Reserved! | Powered by Vitepress with Theme
-    <a href="https://github.com/clark-cui/vitepress-blog-zaun" target="_blank" title="作者">vitepress-blog-zaun</a>
+    <div>
+      <p xmlns:cc="http://creativecommons.org/ns#" class="cc-license"><a rel="cc:attributionURL"
+          href="https://ibert.me">The website
+          content</a> by <a rel="cc:attributionURL dct:creator" property="cc:attributionName"
+          href="https://ibert.me">Herbert He</a> is
+        licensed under <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/?ref=chooser-v1" target="_blank"
+          rel="license noopener noreferrer" style="display:inline-flex;">CC BY-NC-SA 4.0<img
+            style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"><img
+            style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+            src="https://mirrors.creativecommons.org/presskit/icons/by.svg?ref=chooser-v1"><img
+            style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+            src="https://mirrors.creativecommons.org/presskit/icons/nc.svg?ref=chooser-v1"><img
+            style="height:22px!important;margin-left:3px;vertical-align:text-bottom;"
+            src="https://mirrors.creativecommons.org/presskit/icons/sa.svg?ref=chooser-v1"></a></p>
+    </div>
+    <div>
+      Copyright &copy; 2018-{{ date }} <a href="https://github.com/HerbertHe" target="_blank" title="作者">Herbert He</a>
+      All
+      Rights Reserved! | Powered by Vitepress with Theme
+      <a href="https://github.com/clark-cui/vitepress-blog-zaun" target="_blank" title="作者">vitepress-blog-zaun</a>
+    </div>
   </footer>
 </template>
 <script lang="ts" setup>
