1d1797b6a99d5b5d780dc902c842bf1773c27ed4
HerbertHe <Herbert.He0229@gmail.com>
2024-03-31 17:21:44 +0800

    chore :wrench: : mv gitalk to giscus

--- .vitepress/theme/components/Comments.vue
+++ .vitepress/theme/components/Comments.vue
@@ -1,34 +1,8 @@
 <template>
-  <div id="gitalk-container"></div>
+  <Giscus id="giscus-comments" repo="HerbertHe/HerbertHe.github.io" repoId="MDEwOlJlcG9zaXRvcnkxNDA0NDYyNzA="
+    category="Ideas" categoryId="DIC_kwDOCF8KPs4CeWqE" mapping="pathname" strict="0" lang="en" loading="lazy"
+    inputPosition="top" theme="noborder_light" crossorigin="anonymous" reactionsEnabled="1" />
 </template>
 <script lang="ts" setup>
-import "gitalk/dist/gitalk.css";
-import Gitalk from "gitalk";
-import { onContentUpdated, useRouter } from "vitepress";
-
-// const { route, go } = useRouter();
-function deleteChild() {
-  const element = document.querySelector("#gitalk-container");
-  let child = element?.lastElementChild;
-  while (child) {
-    element?.removeChild(child);
-    child = element?.lastElementChild;
-  }
-}
-onContentUpdated(() => {
-  // reset gittalk element for update
-  deleteChild();
-  const gitalk = new Gitalk({
-    clientID: "fc6169aee8aca4748b46",
-    clientSecret: "8f0019b48501a32684d5ca6a9d47618601bf0cef",
-    repo: "blog-gitalk",
-    owner: "HerbertHe",
-    admin: ["HerbertHe"],
-    id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
-    language: "en",
-    distractionFreeMode: true, // Facebook-like distraction free mode
-  });
-  gitalk.render("gitalk-container");
-});
+import Giscus from "@giscus/vue"
 </script>
-<style scoped></style>
